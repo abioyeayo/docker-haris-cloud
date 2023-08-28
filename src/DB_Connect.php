8ff27c946a28ff1df00693d9e57897504f345e40
Ayodeji Abioye <ayo@Ayodejis-MacBook-Pro.local>
2023-08-28 04:24:59 +0100

    updated docker set up for verification study

--- src/DB_Connect.php
+++ src/DB_Connect.php
@@ -4,11 +4,16 @@ date_default_timezone_set('Europe/London');
 class DB_Connect {
     
     public function connect() {
-         define("DB_HOST", "localhost");
-         define("DB_USER", "hutsim_web");
-         define("DB_PASSWORD", "JubyY5!WtV0N");
-         define("DB_DATABASE", "uos-hutsim-cloud-db");
-         define("DB_PORT", 3308);
+        //  define("DB_HOST", "localhost");
+        //  define("DB_USER", "hutsim_web");
+        //  define("DB_PASSWORD", "JubyY5!WtV0N");
+        //  define("DB_DATABASE", "uos-hutsim-cloud-db");
+        //  define("DB_PORT", 3308);
+         define("DB_HOST", "mysql");
+         define("DB_USER", "helloworld");
+         define("DB_PASSWORD", "<db-password>");
+         define("DB_DATABASE", "helloworld");
+         define("DB_PORT", 3306);
          
          $con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT)or die(mysqli_connect_error());
          $con->set_charset("utf8");
